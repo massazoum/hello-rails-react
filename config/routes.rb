d05b0dbcc4a637371540d7fe3ef1f89f855d206e
massa <zoumaniguimassa12@gmail.com>
2023-08-22 18:26:27 +0000

    rails generate controller Pages index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   namespace :api do
     get 'greetings/random'
   end
